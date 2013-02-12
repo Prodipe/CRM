dece6f3df2d04f01cc647f101364bafe44eefba4
Neylor <neyloroliveira@nitead.com.br>
2013-02-12 01:06:15 -0300

    Pagina Inicial

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -25,7 +25,9 @@
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
-	Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
+	// Definindo a página inicial 
+	//Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
+	Router::connect('/', array('controller' => 'empresas', 'action' => 'index'));
 /**
  * ...and connect the rest of 'Pages' controller's urls.
  */
